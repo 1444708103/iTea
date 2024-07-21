0fa353161cc4335c9b9570678baa23caa431c826
彭曦阳 <1444708103@qq.com>
2024-07-21 18:42:33 +0800

    init

--- src/main/java/top/season66/itea/model/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package top.season66.itea.model;
-
-public enum OrderStatus {
-    ORDERED, PAID, MAKING, FINISHED, TAKEN;
-}
--- src/main/java/top/season66/itea/model/Size.java
+++
@@ -1,5 +0,0 @@
-package top.season66.itea.model;
-
-public enum Size{
-    SMALL,MEDIUM,LARGE
-}
